e4b7420949025841a48b736ec8d9450298c49379
Djoa1 <angelon.jc@outlook.com>
2022-02-11 11:54:39 -0300

    next frames

--- assets/script.js
+++ assets/script.js
@@ -35,12 +35,12 @@ var touch = {
 
 class Player {
     constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = 50
-    this.color = color
-}
-    draw () {
+        this.x = x
+        this.y = y
+        this.radius = 50
+        this.color = color
+    }
+    draw() {
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         ctx.fillStyle = "#CCCCCC"
@@ -53,36 +53,37 @@ class Player {
 }
 var xPlayer
 var yPlayer
-(innerWidth/10)*(Math.floor(Math.random()*9)+1),
-(innerHeight/10)*(Math.floor(Math.random()*9)+1),
 
-//(innerWidth * Math.random()),(innerHeight * Math.random()),
+    //(innerWidth * Math.random()),
+    //(innerHeight * Math.random()),
+    //(innerWidth/10)*(Math.floor(Math.random()*9)+1),
+    //(innerHeight/10)*(Math.floor(Math.random()*9)+1),
 
-function produzirFrame (){
-    
-    const player = new Player (
-        (innerWidth/10)*(Math.floor(Math.random()*9)+1),
-        (innerHeight/10)*(Math.floor(Math.random()*9)+1),
-        50,
-        ('#' + Math.floor(Math.random()*16777215).toString(16)))
+    function produzirFrame() {
+
+        const player = new Player(
+            ((innerWidth / 10) * (Math.floor(Math.random() * 9) + 1)),
+            ((innerHeight / 10) * (Math.floor(Math.random() * 9) + 1)),
+            50,
+            ('#' + Math.floor(Math.random() * 16777215).toString(16)))
         xPlayer = player.x
         yPlayer = player.y
-    var frame = setInterval(() => {
-        player.radius = player.radius - 1
-        player.draw()
-         if (player.radius == 5) {
-            clearInterval(frame)
-            ctx.clearRect(0, 0, innerWidth, innerHeight)  
-        } else if (acerto) {
-            clearInterval(frame)
-            ctx.clearRect(0, 0, innerWidth, innerHeight)
-            produzirFrame ()
-            acerto = false  
-        }
-    }, timeFrame -= 5)
+        var frame = setInterval(() => {
+            player.radius = player.radius - 1
+            player.draw()
+            if (player.radius == 5) {
+                clearInterval(frame)
+                ctx.clearRect(0, 0, innerWidth, innerHeight)
+            } else if (acerto) {
+                clearInterval(frame)
+                ctx.clearRect(0, 0, innerWidth, innerHeight)
+                produzirFrame()
+                acerto = false
+            }
+        }, timeFrame -= 5)
     }
 
-    //ctx.clearRect(0, 0, innerWidth, innerHeight)
+//ctx.clearRect(0, 0, innerWidth, innerHeight)
 
 
 /*function produzirFrame (){
@@ -102,55 +103,56 @@ var frame = setInterval(() => {
 }, 100);
 }*/
 
-  var acerto = false
-  var xMouse
-  var yMouse
-  canvas.addEventListener('click', (event) => {
-      const react = canvas.getBoundingClientRect()
-      xMouse =  event.clientX - react.left
-      yMouse = event.clientY - react.top
-      acertou(xMouse, yMouse, xPlayer, yPlayer)
-  })
+var acerto = false
+var xMouse
+var yMouse
+canvas.addEventListener('click', (event) => {
+    const react = canvas.getBoundingClientRect()
+    xMouse = event.clientX - react.left
+    yMouse = event.clientY - react.top
+    acertou(xMouse, yMouse, xPlayer, yPlayer)
+})
 
-  function acertou (x1, y1, x2, y2){
-    compareXUm = (Math.floor(x1/100))
-    compareXDois = (Math.floor(x2/100))
-    compareYUm = (Math.floor(y1/100))
-    compareYDois = (Math.floor(y2/100))
+function acertou(x1, y1, x2, y2) {
+    compareXUm = (Math.floor(x1 / 100))
+    compareXDois = (Math.floor(x2 / 100))
+    compareYUm = (Math.floor(y1 / 100))
+    compareYDois = (Math.floor(y2 / 100))
 
-    if (compareXUm == compareXDois & compareYUm == compareYDois){
+    if (compareXUm == compareXDois & compareYUm == compareYDois) {
         console.log("acertou")
         acerto = true
         pnt.innerHTML = parseInt(pnt.innerHTML) + 1
-        
+
 
     }
-  }
-  var timeFrame = 100
-  function start() {
+}
+var timeFrame
+function start() {
     timeFrame = 100
+    pnt.innerHTML = 0
     produzirFrame()
-  }
+}
 
-  /*
-  function getMousePosition(canvas, event) {
-    let rect = canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-    console.log("Coordinate x: " + x, 
-                "Coordinate y: " + y);
+/*
+function getMousePosition(canvas, event) {
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+  console.log("Coordinate x: " + x, 
+              "Coordinate y: " + y);
 }
 
 let canvasElem = document.querySelector("canvas");
-  
+ 
 canvasElem.addEventListener("mousedown", function(e)
 {
-    getMousePosition(canvasElem, e);
+  getMousePosition(canvasElem, e);
 });
 
 if (touch.radius == 0){   
-    ctx.clearRect(0, 0, innerWidth, innerHeight)     
-    touch.radius = 50
-    touch.y = (innerHeight/10)*(Math.floor(Math.random()*9)+1)
-    touch.x = (innerWidth/10)*(Math.floor(Math.random()*9)+1)
-    touch.color = ('#' + Math.floor(Math.random()*16777215).toString(16))}*/
+  ctx.clearRect(0, 0, innerWidth, innerHeight)     
+  touch.radius = 50
+  touch.y = (innerHeight/10)*(Math.floor(Math.random()*9)+1)
+  touch.x = (innerWidth/10)*(Math.floor(Math.random()*9)+1)
+  touch.color = ('#' + Math.floor(Math.random()*16777215).toString(16))}*/
