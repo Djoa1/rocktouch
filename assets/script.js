cc07fc37a8f2b58f9e53eb2c911541792908559a
djao1 <angelon.jc@outlook.com>
2022-02-11 18:24:20 -0300

    commint js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-var canvas = document.createElement('canvas')
 //adicionar classe no elemento criado pra atribuir estilos
-canvas.className = ('canvas')
+var canvas = document.createElement('canvas')
 //anexa no body
+canvas.className = ('canvas')
 canvas.width = (window.innerWidth-10)
 canvas.height = (window.innerHeight-80)
+//atribuir metodo 2d na canvas pra conseguir desenhar
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d')
 
@@ -11,28 +12,24 @@ if (canvas.getContext) {
     window.alert('sem suporte para o navegador')
 }
 document.body.appendChild(canvas)
-//document.body.innerHTML += '<canvas class="canvas"></canvas>'
-//document.getElementsByClassName('canvas').innerHTML = '<canvas id="someId"></canvas>';
 var pnt = document.querySelector('#pontos')
 
-/*
-var touch = {
-    x: innerWidth/2,
-    y: innerHeight/2,
-    radius: 50,
-    color: 'blue',
-    draw: function () {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        ctx.fillStyle = "#CCCCCC"
-        ctx.fill()
-        ctx.lineWidth = 10
-        ctx.strokeStyle = this.color
-        ctx.stroke()
-        ctx.closePath()
-    }
-}*/
-
+//x e y onde o circulo é gerado
+var xPlayer
+var yPlayer
+//x e y onde foi clicado
+var xMouse
+var yMouse
+//controle de acerto
+var acerto = false
+//time e função de inicio
+var timeFrame
+function start() {
+    timeFrame = 100
+    pnt.innerHTML = 0
+    produzirFrame()
+}
+//instancia um objeto para ser usado
 class Player {
     constructor(x, y, radius, color) {
         this.x = x
@@ -51,42 +48,82 @@ class Player {
         ctx.closePath()
     }
 }
-var xPlayer
-var yPlayer
+//função que gera a redução do circulo, encerra e marca ponto
+function produzirFrame() {
 
+    const player = new Player(
+        (((innerWidth-10)/ 10) * (Math.floor(Math.random() * 9) + 1)),
+        (((innerHeight-60) / 10) * (Math.floor(Math.random() * 9) + 1)),
+        50,
+        ('#' + Math.floor(Math.random() * 16777215).toString(16))
+        )
+    xPlayer = player.x
+    yPlayer = player.y
+        var frame = setInterval(() => {
+        player.radius = player.radius - 1
+        player.draw()
+        if (player.radius == 5) {
+            clearInterval(frame)
+            ctx.clearRect(0, 0, innerWidth, innerHeight)
+        } else if (acerto) {
+            clearInterval(frame)
+            ctx.clearRect(0, 0, innerWidth, innerHeight)
+            produzirFrame()
+            acerto = false
+            }
+        }, timeFrame -= 5)
+    }
+//função/metodo para relacionar os encontros de Xs e Ys (toque e circulo gerado)
+canvas.addEventListener('click', (event) => {
+    const react = canvas.getBoundingClientRect()
+    xMouse = event.clientX - react.left
+    yMouse = event.clientY - react.top
+    acertou(xMouse, yMouse, xPlayer, yPlayer)
+})
+//função marca ponto
+function acertou(x1, y1, x2, y2) {
+    compareXUm = (Math.floor(x1 / 100))
+    compareXDois = (Math.floor(x2 / 100))
+    compareYUm = (Math.floor(y1 / 100))
+    compareYDois = (Math.floor(y2 / 100))
+
+    if (compareXUm == compareXDois & compareYUm == compareYDois) {
+        if (((parseInt(pnt.innerHTML))%2)==0){
+        document.getElementById('pontos').style.backgroundColor = '#ff6347'
+        } else {
+        document.getElementById('pontos').style.backgroundColor = '#ffff00'
+    }
+        
+        console.log("acertou")
+        acerto = true
+        pnt.innerHTML = parseInt(pnt.innerHTML) + 1
+    }
+}
+
+/* codigos deixado de lado ou substituido no processo de criação
+//document.body.innerHTML += '<canvas class="canvas"></canvas>'
+//document.getElementsByClassName('canvas').innerHTML = '<canvas id="someId"></canvas>';
     //(innerWidth * Math.random()),
     //(innerHeight * Math.random()),
     //(innerWidth/10)*(Math.floor(Math.random()*9)+1),
     //(innerHeight/10)*(Math.floor(Math.random()*9)+1),
-
-    function produzirFrame() {
-
-        const player = new Player(
-            (((innerWidth-10)/ 10) * (Math.floor(Math.random() * 9) + 1)),
-            (((innerHeight-60) / 10) * (Math.floor(Math.random() * 9) + 1)),
-            50,
-            ('#' + Math.floor(Math.random() * 16777215).toString(16)))
-        xPlayer = player.x
-        yPlayer = player.y
-        var frame = setInterval(() => {
-            player.radius = player.radius - 1
-            player.draw()
-            if (player.radius == 5) {
-                clearInterval(frame)
-                ctx.clearRect(0, 0, innerWidth, innerHeight)
-            } else if (acerto) {
-                clearInterval(frame)
-                ctx.clearRect(0, 0, innerWidth, innerHeight)
-                produzirFrame()
-                acerto = false
-            }
-        }, timeFrame -= 5)
+var touch = {
+    x: innerWidth/2,
+    y: innerHeight/2,
+    radius: 50,
+    color: 'blue',
+    draw: function () {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = "#CCCCCC"
+        ctx.fill()
+        ctx.lineWidth = 10
+        ctx.strokeStyle = this.color
+        ctx.stroke()
+        ctx.closePath()
     }
-
-//ctx.clearRect(0, 0, innerWidth, innerHeight)
-
-
-/*function produzirFrame (){
+}
+function produzirFrame (){
 var frame = setInterval(() => {
     touch.radius = touch.radius - 1
     touch.draw()
@@ -101,40 +138,11 @@ var frame = setInterval(() => {
         touch.color = ('#' + Math.floor(Math.random()*16777215).toString(16))   
     }
 }, 100);
-}*/
-
-var acerto = false
-var xMouse
-var yMouse
-canvas.addEventListener('click', (event) => {
-    const react = canvas.getBoundingClientRect()
-    xMouse = event.clientX - react.left
-    yMouse = event.clientY - react.top
-    acertou(xMouse, yMouse, xPlayer, yPlayer)
-})
-
-function acertou(x1, y1, x2, y2) {
-    compareXUm = (Math.floor(x1 / 100))
-    compareXDois = (Math.floor(x2 / 100))
-    compareYUm = (Math.floor(y1 / 100))
-    compareYDois = (Math.floor(y2 / 100))
-
-    if (compareXUm == compareXDois & compareYUm == compareYDois) {
-        console.log("acertou")
-        acerto = true
-        pnt.innerHTML = parseInt(pnt.innerHTML) + 1
-
-
-    }
-}
-var timeFrame
-function start() {
-    timeFrame = 100
-    pnt.innerHTML = 0
-    produzirFrame()
 }
 
-/*
+
+
+
 function getMousePosition(canvas, event) {
   let rect = canvas.getBoundingClientRect();
   let x = event.clientX - rect.left;
